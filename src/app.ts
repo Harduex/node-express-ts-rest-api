2867af21fd9dcff438dbd1203fd91e3abc15b58f
Harduex <simeon.simka@gmail.com>
2021-10-16 01:28:07 +0300

    npm init and express with typescript setup, .env created

---
+++ src/app.ts
@@ -0,0 +1,17 @@
+
+  
+import express from "express";
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const port = process.env.PORT || 3000;
+
+const app = express();
+
+app.use(express.json());
+
+
+app.listen(port, () => {
+  console.log(`App is running at http://localhost:${port}`);
+});
