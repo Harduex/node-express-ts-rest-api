ef1406b489c9455b3cebe2d1b94d9cdafe0e6819
Harduex <simeon.simka@gmail.com>
2021-10-16 16:57:05 +0300

    build folder added to .gitignore

--- build/config/local.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.default = {
-    server: {
-        port: process.env.PORT || 3000,
-        host: process.env.HOST || "localhost",
-    },
-};
--- build/config/production.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.default = {
-    server: {
-        port: 8080,
-        host: "0.0.0.0",
-    },
-};
